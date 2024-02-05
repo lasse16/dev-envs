82c5d66bdecc80fadeafb0a96ab1b1dbda15bc93
Lasse Haffke <lasse@haffke.me>
2024-02-05 15:53:40 +0100

    feat: specify terraform version

--- flake.nix
+++ flake.nix
@@ -3,6 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  nixConfig = {
+    extra-substituters = "https://nixpkgs-terraform.cachix.org";
+    extra-trusted-public-keys = "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw=";
   };
 
   outputs = { self, nixpkgs }:
@@ -10,6 +17,7 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        terraform = nixpkgs-terraform.packages.${system}."1.7.1";
       });
     in
     {
